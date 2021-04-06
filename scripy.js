b281c1861da5f95a8e3fbb49304f0018c4ab7661
lilia-feknous <63005107+lilia-feknous@users.noreply.github.com>
2021-04-06 14:29:42 +0100

    Add files via upload

---
+++ scripy.js
@@ -0,0 +1,15 @@
+var x= documeent.getElementsById("login");
+var y= documeent.getElementsById("register");
+var z= documeent.getElementsById("btn");
+
+function register(){
+  x.style.left = "-400px";
+  y.style.left = "50px";
+  z.style.left = "110px";
+}
+
+function login(){
+x.style.left = "50px";
+y.style.left = "450px";
+z.style.left = "0";
+}
